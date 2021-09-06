1adc7f17b6fab4545a60d87f524dd2e74957c8a4
ahramjuniors <Ahramjuniors@gmail.com>
2021-09-07 00:17:12 +0200

    added new files

---
+++ js/Rotation.js
@@ -0,0 +1,9 @@
+function charger()
+{
+
+    setTimeout(function(){
+        document.getElementById("everything").style.display="block";
+        document.getElementById("loader").style.display="none";
+    }, 3000);//
+
+}
